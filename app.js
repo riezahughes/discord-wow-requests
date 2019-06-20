68631f1207ba1904bca046bf50f37438f01c3349
Dan <dan@ekwholesale.co.uk>
2019-06-20 20:59:40 +0100

    Bare minimum checks in app.js

--- app.js
+++ app.js
@@ -7,9 +7,21 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
+
+    if(!msg.author.bot){
+        if(msg.content.includes("!request")){
+
+            msg.reply("Request");
+        }
+
+        if(msg.content.includes("!deposit")){
+            msg.reply("Deposit");
+        }
+
+        if(msg.content.includes("!myrequests")){
+            msg.reply("list all requests");
+        }
+    }
 });
 
 client.login(process.env.DISCORDBOTTOKEN);
--- functions/viewRequests.js
+++ functions/viewRequests.js
@@ -2,7 +2,7 @@
 
 /*
 
-A user will use the syntax "!viewrequests"
+A user will use the syntax "!myrequests"
 There will be a check on the table of all requests from the user that aren't completed.
 The bot will respond to the user with a quick summary of whats currently on the board.
 
