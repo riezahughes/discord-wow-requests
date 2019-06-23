0a753f6bc3cfa274cd6359cde874e31907fc9fbf
Dan <dan@ekwholesale.co.uk>
2019-06-23 20:15:22 +0100

    Quick deposit check for messages

--- app.js
+++ app.js
@@ -142,7 +142,14 @@ client.on("message", async (msg) => {
   }
 
   if (commandSplit === "!deposit") {
-    msg.reply(deposit.createDeposit(shoppingList, shoppingQuantity))
+    const itemdeposit = await deposit.createDeposit(shoppingList, shoppingQuantity);
+    console.log(itemdeposit);
+    if (itemdeposit.fulfilled === true) {
+      msg.reply("completed");
+    } else {
+      msg.reply("more to go");
+    }
+
   }
 })
 
