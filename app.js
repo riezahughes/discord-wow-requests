2d645f28c285df21d87ece8a4b4ff61cf6ec7fc4
sashman <sashman90@gmail.com>
2019-06-23 20:12:56 +0100

    Early return

--- app.js
+++ app.js
@@ -45,103 +45,104 @@ const createEmbed = (
 //when the bot boots
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`)
-  client.user.setPresence({ game: { name: "With yer maw", type: 0 } });
+  client.user.setPresence({ game: { name: "With yer maw", type: 0 } })
 })
 
 //on a message (by default it checks every channel)
 client.on("message", async (msg) => {
-  if (!msg.author.bot) {
+  if (msg.author.bot) {
+    return
+  }
 
-    //if it's just checking requests
-    if (msg.content.toLowerCase() === "!myrequests") {
-      let userResponse = ""
-      const viewResponse = await view.viewRequests(msg.member.id)
-      if (!viewResponse) {
-        msg.reply("You don't have any pending requests at this time.")
-      } else {
-        userResponse = "```Current Requests```"
-        for (i = 0; i < viewResponse.length; i++) {
-          userResponse +=
-            viewResponse[i].current_quantity +
-            " / " +
-            viewResponse[i].initial_quantity +
-            " of " +
-            viewResponse[i].item_name +
-            " " +
-            "collected.\n"
-        }
-        msg.reply(userResponse)
+  //if it's just checking requests
+  if (msg.content.toLowerCase() === "!myrequests") {
+    let userResponse = ""
+    const viewResponse = await view.viewRequests(msg.member.id)
+    if (!viewResponse) {
+      msg.reply("You don't have any pending requests at this time.")
+    } else {
+      userResponse = "```Current Requests```"
+      for (i = 0; i < viewResponse.length; i++) {
+        userResponse +=
+          viewResponse[i].current_quantity +
+          " / " +
+          viewResponse[i].initial_quantity +
+          " of " +
+          viewResponse[i].item_name +
+          " " +
+          "collected.\n"
       }
+      msg.reply(userResponse)
     }
+  }
 
-    //otherwise take the string that the user has entered. Split it into two pieces. The command and the items/values
-    //Make the command lowercase for easy checks.
+  //otherwise take the string that the user has entered. Split it into two pieces. The command and the items/values
+  //Make the command lowercase for easy checks.
 
-    let commandSplit = msg.content.substr(0, msg.content.indexOf(" "))
-    commandSplit = commandSplit.toLowerCase()
+  let commandSplit = msg.content.substr(0, msg.content.indexOf(" "))
+  commandSplit = commandSplit.toLowerCase()
 
-    let requestItems = msg.content.substr(msg.content.indexOf(" ") + 1)
+  let requestItems = msg.content.substr(msg.content.indexOf(" ") + 1)
 
-    let shoppingQuantity = requestItems.substr(0, requestItems.indexOf(" "))
-    let shoppingList = requestItems.substr(requestItems.indexOf(" ") + 1)
+  let shoppingQuantity = requestItems.substr(0, requestItems.indexOf(" "))
+  let shoppingList = requestItems.substr(requestItems.indexOf(" ") + 1)
 
-    //quick test to show things going through correctly.
-    // console.log(
-    //   commandSplit +
-    //   " of " +
-    //   requestItems +
-    //   " from " +
-    //   msg.member.user.tag +
-    //   " ( id:" +
-    //   msg.member.id +
-    //   " )"
-    // )
+  //quick test to show things going through correctly.
+  // console.log(
+  //   commandSplit +
+  //   " of " +
+  //   requestItems +
+  //   " from " +
+  //   msg.member.user.tag +
+  //   " ( id:" +
+  //   msg.member.id +
+  //   " )"
+  // )
 
-    if (commandSplit === "!request" && !isNaN(shoppingQuantity)) {
-      const setupMessage = await client.channels
-        .get(process.env.BOTCHANNEL)
-        .send("[processing]")
+  if (commandSplit === "!request" && !isNaN(shoppingQuantity)) {
+    const setupMessage = await client.channels
+      .get(process.env.BOTCHANNEL)
+      .send("[processing]")
 
-      const runRequest = await request.createRequest(
+    const runRequest = await request.createRequest(
+      msg.member.id,
+      setupMessage.id,
+      shoppingList,
+      shoppingQuantity
+    )
+
+    if (runRequest) {
+      console.log(runRequest)
+
+      const richembed = createEmbed(
+        runRequest[0].id,
         msg.member.id,
-        setupMessage.id,
         shoppingList,
         shoppingQuantity
       )
 
-      if (runRequest) {
-        console.log(runRequest)
+      try {
+        const message = await client.channels
+          .get(process.env.BOTCHANNEL)
+          .fetchMessage(setupMessage.id)
 
-        const richembed = createEmbed(
-          runRequest[0].id,
-          msg.member.id,
-          shoppingList,
-          shoppingQuantity
+        await message.edit({ embed: richembed })
+
+        const returnResponse =
+          "Request Created! :id:: ` " + runRequest[0].id + " `"
+        msg.reply(
+          `${returnResponse}! Check <#${process.env.BOTCHANNEL}> for post`
         )
-
-        try {
-
-          const message = await client.channels
-            .get(process.env.BOTCHANNEL)
-            .fetchMessage(setupMessage.id)
-
-          await message.edit({ embed: richembed })
-
-          const returnResponse = "Request Created! :id:: ` " + runRequest[0].id + " `"
-          msg.reply(
-            `${returnResponse}! Check <#${process.env.BOTCHANNEL}> for post`
-          )
-        } catch (e) {
-          console.error(e)
-        }
-      } else {
-        msg.reply("Format incorrect. Please try again. `!request [qty] [item]`")
+      } catch (e) {
+        console.error(e)
       }
+    } else {
+      msg.reply("Format incorrect. Please try again. `!request [qty] [item]`")
     }
+  }
 
-    if (commandSplit === "!deposit") {
-      msg.reply(deposit.createDeposit(shoppingList, shoppingQuantity));
-    }
+  if (commandSplit === "!deposit") {
+    msg.reply(deposit.createDeposit(shoppingList, shoppingQuantity))
   }
 })
 
