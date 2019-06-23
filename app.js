7d1e53f314ca1304b8ddbbd29a4df60a1131017a
sashman <sashman90@gmail.com>
2019-06-23 14:53:57 +0100

    Some refactoring

--- app.js
+++ app.js
@@ -10,119 +10,133 @@ const request = require("./functions/createRequest")
 const deposit = require("./functions/createDeposit")
 const view = require("./functions/viewRequests")
 
+const createEmbed = (
+  setupMessageId,
+  memberId,
+  shoppingList,
+  shoppingQuantity
+) => ({
+  title: `:id: \`${setupMessageId}\` - New Request From: `,
+  description: `\n\n<@${memberId}>`,
+  color: 5508893,
+  timestamp: "2019-06-20T22:07:57.142Z",
+  footer: {
+    icon_url: "https://cdn.discordapp.com/embed/avatars/0.png",
+    text: "Be excellent to each other"
+  },
+  thumbnail: {
+    url: "https://cdn.discordapp.com/embed/avatars/0.png"
+  },
+
+  fields: [
+    {
+      name: "Item",
+      value: `\`\`\`${shoppingList}\`\`\``,
+      inline: true
+    },
+    {
+      name: "Quantity Needed:",
+      value: `\`\`\`${shoppingQuantity}\`\`\``,
+      inline: true
+    }
+  ]
+})
+
 //when the bot boots
 client.on("ready", () => {
-    console.log(`Logged in as ${client.user.tag}!`)
+  console.log(`Logged in as ${client.user.tag}!`)
 })
 
 //on a message (by default it checks every channel)
 client.on("message", async (msg) => {
-    if (!msg.author.bot) {
-        //msg.content.includes
-        //if it's just checking requests
-        if (msg.content.toLowerCase() === "!myrequests") {
+  if (!msg.author.bot) {
+    //msg.content.includes
+    //if it's just checking requests
+    if (msg.content.toLowerCase() === "!myrequests") {
+      let userResponse = ""
+      const viewResponse = await view.viewRequests(msg.member.id)
 
-            let userResponse = "";
-            const viewResponse = await view.viewRequests(msg.member.id)
-
-            if (!viewResponse.length) {
-                msg.reply("You don't have any pending requests at this time.");
-            } else {
-                userResponse = "```Current Requests```";
-                for (i = 0; i < viewResponse.length; i++) {
-                    userResponse += viewResponse[i].current_quantity + " / " + viewResponse[i].current_quantity + " of " + viewResponse[i].item_name + " " + "collected.\n";
-                }
-                msg.reply(userResponse)
-            }
-
-        }
-
-        //otherwise take the string that the user has entered. Split it into two pieces. The command and the items/values
-        //Make the command lowercase for easy checks.
-
-        let commandSplit = msg.content.substr(0, msg.content.indexOf(" "))
-        commandSplit = commandSplit.toLowerCase()
-
-        let requestItems = msg.content.substr(msg.content.indexOf(" ") + 1)
-
-        let shoppingQuantity = requestItems.substr(0, requestItems.indexOf(" "))
-        let shoppingList = requestItems.substr(requestItems.indexOf(" ") + 1)
-
-        //quick test to show things going through correctly.
-        console.log(
-            commandSplit +
+      if (!viewResponse.length) {
+        msg.reply("You don't have any pending requests at this time.")
+      } else {
+        userResponse = "```Current Requests```"
+        for (i = 0; i < viewResponse.length; i++) {
+          userResponse +=
+            viewResponse[i].current_quantity +
+            " / " +
+            viewResponse[i].current_quantity +
             " of " +
-            requestItems +
-            " from " +
-            msg.member.user.tag +
-            " ( id:" +
-            msg.member.id +
-            " )"
+            viewResponse[i].item_name +
+            " " +
+            "collected.\n"
+        }
+        msg.reply(userResponse)
+      }
+    }
+
+    //otherwise take the string that the user has entered. Split it into two pieces. The command and the items/values
+    //Make the command lowercase for easy checks.
+
+    let commandSplit = msg.content.substr(0, msg.content.indexOf(" "))
+    commandSplit = commandSplit.toLowerCase()
+
+    let requestItems = msg.content.substr(msg.content.indexOf(" ") + 1)
+
+    let shoppingQuantity = requestItems.substr(0, requestItems.indexOf(" "))
+    let shoppingList = requestItems.substr(requestItems.indexOf(" ") + 1)
+
+    //quick test to show things going through correctly.
+    console.log(
+      commandSplit +
+        " of " +
+        requestItems +
+        " from " +
+        msg.member.user.tag +
+        " ( id:" +
+        msg.member.id +
+        " )"
+    )
+
+    if (commandSplit === "!request" && !isNaN(shoppingQuantity)) {
+      const setupMessage = await client.channels
+        .get(process.env.BOTCHANNEL)
+        .send("[processing]")
+
+      console.dir("!!!!!!! " + setupMessage.id + " !!!!")
+
+      const runRequest = await request.createRequest(
+        msg.member.id,
+        setupMessage.id,
+        shoppingList,
+        shoppingQuantity
+      )
+
+      if (runRequest) {
+        console.log(runRequest)
+
+        const richembed = createEmbed(
+          setupMessage.id,
+          msg.member.id,
+          shoppingList,
+          shoppingQuantity
         )
 
-        if (commandSplit === "!request" && !isNaN(shoppingQuantity)) {
-            const setupMessage = await client.channels.get(process.env.BOTCHANNEL).send("[processing]");
+        const message = await msg.channel.fetchMessage(setupMessage.id)
+        await message.edit(richembed)
 
-            console.dir("!!!!!!! " + setupMessage.id + " !!!!");
-
-            const runRequest = await request.createRequest(
-                msg.member.id,
-                setupMessage.id,
-                shoppingList,
-                shoppingQuantity
-            );
-
-
-            if (typeof setupMessage !== "undefined") {
-
-                console.log(runRequest);
-
-                let richembed = {
-                    title: ":id: `" + setupMessage.id + "` - New Request From: ",
-                    description: "\n\n<@" + msg.member.id + ">",
-                    color: 5508893,
-                    timestamp: "2019-06-20T22:07:57.142Z",
-                    footer: {
-                        icon_url: "https://cdn.discordapp.com/embed/avatars/0.png",
-                        text: "Be excellent to each other"
-                    },
-                    thumbnail: {
-                        url: "https://cdn.discordapp.com/embed/avatars/0.png"
-                    },
-
-                    fields: [
-                        {
-                            name: "Item",
-                            value: "```" + shoppingList + "```",
-                            inline: true
-                        },
-                        {
-                            name: "Quantity Needed:",
-                            value: "```" + shoppingQuantity + "```",
-                            inline: true
-                        }
-                    ]
-                }
-                msg.channel.fetchMessages({ around: setupMessage.id, limit: 1 })
-                    .then(msg => {
-                        const fetchedMsg = msg.first();
-                        fetchedMsg.edit(richembed);
-                    });
-                //client.channels.get(process.env.BOTCHANNEL).send({ embed: richembed })
-
-                let returnResponse = "Request Created! :id:: ` " + runRequest.id + " `"
-                msg.reply(
-                    `${returnResponse}! Check <#${process.env.BOTCHANNEL}> for post`
-                )
-            } else {
-                msg.reply("Format incorrect. Please try again. `!request [qty] [item]`")
-            }
-        }
-
-        if (commandSplit === "!deposit") {
-            msg.reply(deposit.createDeposit())
-        }
+        let returnResponse = "Request Created! :id:: ` " + runRequest.id + " `"
+        msg.reply(
+          `${returnResponse}! Check <#${process.env.BOTCHANNEL}> for post`
+        )
+      } else {
+        msg.reply("Format incorrect. Please try again. `!request [qty] [item]`")
+      }
     }
+
+    if (commandSplit === "!deposit") {
+      msg.reply(deposit.createDeposit())
+    }
+  }
 })
 
 //log in the bot via the bot token from discord.
